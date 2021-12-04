9a2175955df662a843310ce828f39dc83b3e2ff7
Nicolas <nicolas.touzard-ext@st.com>
2021-12-04 07:39:59 +0100

    Initial commit with nest+angular+colyseus

---
+++ apps/nx-colyseus-phaser-template/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-colyseus-phaser-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'nx-colyseus-phaser-template';
+
+  test() {
+  }
+}
